4358aed9a103ebfc04781c14668c8cbae2566499
Sergio Veiga <sergioveiga@gmail.com>
2013-09-30 18:24:37 +0200

    port default

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ erl -sname todolist -pa apps/*/ebin -pa deps/*/ebin \
 	-eval "application:start(cowboy)." \
 	-eval "application:start(todolist)." \
 	-eval "io:format(\"~n~nThe following examples are available:~n\")." \
-	-eval "io:format(\"* Now check http://localhost:8080~n\")." \
+	-eval "io:format(\"* Now check http://localhost:8082~n\")." \
 
 
